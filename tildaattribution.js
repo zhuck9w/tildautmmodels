f37247968afa5d76b2f692626d88517e9ecc31ab
zhuck9w <107795901+zhuck9w@users.noreply.github.com>
2023-05-09 16:04:31 +0300

    Update tildaattribution.js

--- tildaattribution.js
+++ tildaattribution.js
@@ -4,27 +4,31 @@ function firstClick() {
     // The TILDAUTM cookie already exists
     return;
   }
-  const expirationDate = new Date();
-    expirationDate.setDate(expirationDate.getDate() + 90);
+
   const utmParams = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content']
     .filter(param => Boolean(new URLSearchParams(window.location.search).get(param)))
     .map(param => `${param}=${encodeURIComponent(new URLSearchParams(window.location.search).get(param))}`);
-
+  
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + 90);
+  
   const referrer = document.referrer;
   const searchEngineRegex = /(google|nova\.rambler|yandex|yahoo|duckduckgo|bing)\./i;
   const referralRegex = /https?:\/\/([^/]+)/i;
 
-  if (referrer.match(searchEngineRegex)) {
-    const searchEngine = referrer.match(searchEngineRegex)[1];
-    utmParams.push(`utm_source=${searchEngine}`);
-    utmParams.push('utm_medium=organic');
-  } else if (referrer.match(referralRegex)) {
-    const referralDomain = referrer.match(referralRegex)[1];
-    utmParams.push(`utm_source=${referralDomain}`);
-    utmParams.push('utm_medium=referral');
-  } else {
-    utmParams.push('utm_source=(direct)');
-    utmParams.push('utm_medium=none');
+  if (utmParams.length === 0) {
+    if (referrer.match(searchEngineRegex)) {
+      const searchEngine = referrer.match(searchEngineRegex)[1];
+      utmParams.push(`utm_source=${searchEngine}`);
+      utmParams.push('utm_medium=organic');
+    } else if (referrer.match(referralRegex)) {
+      const referralDomain = referrer.match(referralRegex)[1];
+      utmParams.push(`utm_source=${referralDomain}`);
+      utmParams.push('utm_medium=referral');
+    } else {
+      utmParams.push('utm_source=(direct)');
+      utmParams.push('utm_medium=none');
+    }
   }
 
   if (utmParams.length > 0) {
